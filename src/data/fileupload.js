a0ba93d909f00cbb377e5a30408c06e197efa382
prakash1921 <dhobi.prakash1920@gmail.com>
2020-09-12 16:39:57 +0530

    changes done for package.json file

--- src/data/fileupload.js
+++ src/data/fileupload.js
@@ -86,6 +86,11 @@ router.get('/getfileById/:id',function(req,res){
     })
 });
 
+router.get('/gethome',function(req,res){
+    var title="Enter into Home Page";
+res.send(title);
+}
+
 router.post('/remove',function(req,res){
     uploaddischarge.remove({_id:req.body.id},(err,response) => {
         if(err){
