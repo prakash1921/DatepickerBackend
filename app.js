b5d4929b5d5a15011a8507bffaa9f8b324cadced
prakash1921 <dhobi.prakash1920@gmail.com>
2020-09-13 16:59:36 +0530

    changes done

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var hbs = require('hbs');
 const mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-// var port = process.env.port || 8000;
+var port = process.env.PORT || 8000;
 // var port = process.env.PORT
-const host = '0.0.0.0';
-const port = process.env.PORT || 8000;
+// const host = '0.0.0.0';
+// const port = process.env.PORT || 8000;
 var app = express();
 var cors = require('cors');
 // view engine setup
@@ -65,10 +65,10 @@ app.use(express.static(path.join(__dirname,"/public/uploads"),{maxAge:3155760000
 // app.engine('hbs', hbs({extname: 'hbs',defaultLayout:'layout',layoutDir:_dirname + '/views/layout/'}));
 // app.set('views',path.join(_dirname,'/views')); //set path in which file is present
 app.set('view engine','hbs');
-// app.listen(port);
-app.listen(port, host, function() {
-  console.log("Server started.......");
-});
+app.listen(port);
+// app.listen(port, host, function() {
+//   console.log("Server started.......");
+// });
 
 //package.json
 // "private": true,
