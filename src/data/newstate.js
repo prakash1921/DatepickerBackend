490dac286a685756d9b83b4e34357eaed4d72ffd
prakash1921 <dhobi.prakash1920@gmail.com>
2020-09-28 00:09:37 +0530

    changes done for script

--- src/data/newstate.js
+++ src/data/newstate.js
@@ -3,13 +3,13 @@ var router = express.Router();
 const path = require('path');
 const fs = require('fs');
 var async = require("async");
-const newStateMode = require('../../model/newStateMode');
+const newStateModel = require('../../model/newStateMode');
 const newCityModel = require('../../model/newCityMode');
 
 var router = express.Router();
 
 router.get('/province/:eid',function(req,res){
-    newStateMode.find({ElectionID:req.params.eid},function(err,response){
+    newStateModel.find({ElectionID:req.params.eid},function(err,response){
         if(err){
             res.send(err);
         }else{
@@ -22,11 +22,14 @@ router.get('/province/:eid',function(req,res){
 router.get('/city/:id',function(req,res){
     newCityModel.find({cityID:req.params.id},function(err,response){
         if(err){
+            console.log("new city data",err)
             res.send(err);
         }else{
+            console.log("new city data",response)
             res.send(response);
         }
     })
 })
+
 
 module.exports= router;
